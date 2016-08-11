68ec55494b6c080406c9dbf101e2bdeb1fc06480
Nazar1997 <halonazar1997@gmail.com>
2016-08-11 23:49:56 +0500

    Add files via upload
    Полностью рабочая версия, не хватает прунинга и она не умеет работать с неполными данными

--- Decision_Tree.cpp
+++ Decision_Tree.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <set> 
+#include <iterator>
 
 using std::cin;
 using std::cout;
@@ -16,16 +17,23 @@ struct Node {
     int main_class = -1; // класс не определен
 };
 
-vector<vector<double>> input_X() {
-    vector<vector<double>> X;
-    return X;
-    //упорядочивание данных
-}
-
-vector<size_t> input_Y() {
-    vector<size_t> Y;
-    return Y;
-    //упорядочивание данных
+void input(vector<vector<double>>& X, vector<size_t>& Y) {
+    size_t i;
+    cin >> i;
+    size_t q;
+    cin >> q;
+    for (size_t w = 0; w != q; ++w) {
+        vector<double> vec;
+        for (size_t r = 0; r != i; ++r) {
+            double a;
+            cin >> a;
+            vec.push_back(a);
+        }
+        X.push_back(vec);
+        size_t clss;
+        cin >> clss;
+        Y.push_back(clss);
+    }
 }
 
 Node *Tree_Construction(const vector<vector<double>>& X, vector<size_t>& Y,const vector<size_t>& classes, const vector<TAtribute>& attributes, vector<size_t>& available_data, size_t depth = 0)
@@ -38,8 +46,8 @@ Node *Tree_Construction(const vector<vector<double>>& X, vector<size_t>& Y,const
     bool is_node_a_leaf = false;
     size_t most_frequensly_class = 0;
     for (size_t i = 0; i != classes.size(); ++i) {
-        if (frequensly[i] / available_data.size() > 0.8) {
-            most_frequensly_class = i;
+        if (frequensly[i] / available_data.size() > 0.9) {
+            most_frequensly_class = classes[i];
             is_node_a_leaf = true;
             break;
         }
@@ -49,18 +57,18 @@ Node *Tree_Construction(const vector<vector<double>>& X, vector<size_t>& Y,const
         the_answer->main_class = most_frequensly_class;
         return the_answer;
     } else {
-        size_t attribute_number = 0; // атрибут который выберем
+        int attribute_number = -1; // атрибут который выберем
         double Gini = -1;
         for (size_t i = 0; i != attributes.size(); ++i) {//анализируем все атрибуты
-            vector<size_t> left_classes(classes.size()), right_classes(classes.size());
+            vector<size_t> left_classes(classes.size(),0), right_classes(classes.size(),0);
             for (size_t q = 0; q != available_data.size(); ++q) {
                 if (X[available_data[q]][attributes[i].first] < attributes[i].second) {
-                    auto it = std::lower_bound(left_classes.begin(), left_classes.end(), Y[available_data[q]]);
-                    ++left_classes[it - left_classes.begin()];
+                    auto it = std::lower_bound(classes.begin(), classes.end(), Y[available_data[q]]);
+                    ++left_classes[it - classes.begin()];
                 }
                 else {
-                    auto it = std::lower_bound(right_classes.begin(), right_classes.end(), Y[available_data[q]]);
-                    ++right_classes[it - right_classes.begin()];
+                    auto it = std::lower_bound(classes.begin(), classes.end(), Y[available_data[q]]);
+                    ++right_classes[it - classes.begin()];
                 }
             }
             size_t left_elements = 0, left_info = 0;
@@ -71,8 +79,8 @@ Node *Tree_Construction(const vector<vector<double>>& X, vector<size_t>& Y,const
                 left_info += left_classes[q] * left_classes[q];
                 right_info += right_classes[q] * right_classes[q];
             }
-            if (Gini < left_elements*left_info + right_elemnts*right_info) {
-                Gini = left_elements*left_info + right_elemnts*right_info;
+            if (left_elements*right_elemnts != 0 && Gini < left_info/ left_elements + right_info/ right_elemnts) {
+                Gini = left_info / left_elements + right_info / right_elemnts;
                 attribute_number = i;
             }
         }
@@ -102,7 +110,7 @@ vector<TAtribute> Making_atrributes(vector<vector<double>>& X) {
     for (size_t i = 0; i != X[0].size(); ++i) {//проходим все атрибуты
         std::set<double> values;
         for (size_t q = 0; q != X.size(); ++q) {//создание коллекции значений
-            values.insert(X[i][q]);
+            values.insert(X[q][i]);///
         }
         auto it = values.begin();
         while (it != --values.end()) {
@@ -112,9 +120,22 @@ vector<TAtribute> Making_atrributes(vector<vector<double>>& X) {
     return attributes;
 }
 
+size_t classificator(const vector<double>& object, Node* Tree) {
+    Node* the_Node = Tree;
+    while (the_Node->main_class==-1) {
+        if (object[the_Node->the_atribute.first] > the_Node->the_atribute.second) {
+            the_Node = the_Node->right;
+        } else {
+            the_Node = the_Node->left;
+        }
+    }
+    return the_Node->main_class;
+}
+
 int main() {
-    vector<vector<double>> X = input_X();//данные на вход
-    vector<size_t> Y = input_Y();//классы
+    vector<vector<double>> X;
+    vector<size_t> Y;
+    input(X, Y);
     vector<TAtribute> attributes = Making_atrributes(X); // создаем атрибуты и пороги
     vector<size_t> available_data(X.size());//укажем всю выборку как доступную
     for (size_t i = 0; i != X.size(); ++i) {
@@ -129,6 +150,11 @@ int main() {
     }
     sort(classes.begin(), classes.end());
     Node *Root = Tree_Construction(X, Y, classes, attributes, available_data);
+    for (size_t i = 0; i != 10; ++i) {
+        vector<double> obj(2);
+        cin >> obj[0] >> obj[1];
+        cout << classificator(obj, Root);
+    }
     return 0;
 }
 
